46b29c007fab49c5934d8f709b35058322b88169
ryurina <ryurina73@gmail.com>
2026-09-17 10:56:19 +0300

    perceptron begin

---
+++ perceptron/perceptron.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+
+/*
+ * input (x1,..., xn)
+ * weights (w1,..., wn)
+ * bias
+ *
+ * z = sum(wi*xi + b)
+ * ŷ = Relu(z)
+ *
+ * error = y - ŷ
+ *
+ * lr (learning rate)
+ *
+ * n_iter (number of iteration)
+ *
+ * */
+
+class Perceptron{
+  public:
+    double lr;
+    double n_iter;
+
+    Perceptron(double lr, double n_iter): a(a), b(b) {}
+};
+
+int main (int argc, char *argv[]) {
+  
+  return 0;
+}
